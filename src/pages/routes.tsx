b12a17dabb9238f3ea3a527dd24da2bc37cfcb28
hauleguitare <alexander.dutyone@gmail.com>
2022-09-27 00:24:58 +0700

    (Login, SignUpPage): Create Login SignUpPage and fixed Styled

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,3 +1,7 @@
+import { useAutoAnimate } from '@formkit/auto-animate/react';
+import AuthProtected from '@src/components/AuthProtected';
+import { AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
 import { Route, Routes } from 'react-router';
 import DiscoverPage from './Discover';
 import HomePage from './Home';
@@ -5,31 +9,64 @@ import LoginPage from './Login';
 import NotFoundPage from './NotFound';
 import RegisterPage from './Register';
 import SearchPage from './Search';
+import TestPage from './Test';
 
 interface IClientRoutesProps {}
 const ClientRoutes: React.FunctionComponent<IClientRoutesProps> = (props) => {
+  const [isLoggedIn, setLoggedIn] = useState(localStorage.getItem('access_token') ? true : false);
   return (
-    <Routes>
-      {/* This is routes for HomePage */}
-      <Route path="/" element={<HomePage />} />
-      {/* This is routes for SearchPage */}
-      <Route path="/search" element={<SearchPage />} />
+    <AnimatePresence>
+      <Routes>
+        {/* This is routes for HomePage */}
+        <Route path="/" element={<HomePage />} />
+        {/* This is routes for SearchPage */}
+        <Route path="/search" element={<SearchPage />} />
 
-      {/* This is routes for DiscoverPage */}
-      <Route path="/movie" element={<DiscoverPage type="movie" />}>
-        <Route path="movie/:movie_id" element={<div>DETAIL MOVIE</div>} />
-      </Route>
+        {/* This is routes for DiscoverPage */}
+        <Route path="/movie" element={<DiscoverPage type="movie" />}>
+          <Route path="movie/:movie_id" element={<div>DETAIL MOVIE</div>} />
+        </Route>
 
-      <Route path="/tv" element={<DiscoverPage type="tv" />}>
-        <Route path="tv/:movie_id" element={<div>DETAIL TV</div>} />
-      </Route>
+        <Route path="/tv" element={<DiscoverPage type="tv" />}>
+          <Route path="tv/:movie_id" element={<div>DETAIL TV</div>} />
+        </Route>
 
-      <Route path="/login" element={<LoginPage />}></Route>
-      <Route path="/register" element={<RegisterPage />}></Route>
+        <Route
+          path="/login"
+          element={
+            <AuthProtected hasLogin={!isLoggedIn} redirectTo={'/'}>
+              <LoginPage />
+            </AuthProtected>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <AuthProtected hasLogin={!isLoggedIn} redirectTo={'/'}>
+              <RegisterPage />
+            </AuthProtected>
+          }
+        />
+        {/* <Route path="/signup" element={<RegisterPage />}></Route> */}
 
-      {/* This is routes for NotFoundPage */}
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+        {/* This is Private routes */}
+        <Route
+          path="/profile"
+          element={
+            <AuthProtected hasLogin={isLoggedIn} redirectTo={'/login'}>
+              <div>THIS IS PROFILE</div>
+            </AuthProtected>
+          }
+        ></Route>
+
+        {/* This is test page */}
+        <Route path="/test" element={<TestPage />}></Route>
+        {/* This is test page */}
+
+        {/* This is routes for NotFoundPage */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </AnimatePresence>
   );
 };
 
---
+++ src/services/context/Auth/index.tsx
@@ -0,0 +1,31 @@
+import { auth } from '@src/services/Firebase';
+import { onAuthStateChanged, User } from 'firebase/auth';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+interface IAuthProviderProps {
+  children?: React.ReactNode;
+}
+
+const AuthContext = createContext<User | null>(null);
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+const AuthProvider: React.FunctionComponent<IAuthProviderProps> = (props) => {
+  const { children } = props;
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, async (result) => {
+      setCurrentUser(result);
+      if (result) {
+        const access_token = await result.getIdToken();
+        localStorage.setItem('access_token', access_token);
+      }
+    });
+    return () => unSubscribe();
+  }, []);
+  return <AuthContext.Provider value={currentUser}>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
