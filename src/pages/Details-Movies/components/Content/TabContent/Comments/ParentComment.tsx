a7cba2afd9385bdac56205977a7b8997a9adcba0
hauleguitare <alexander.dutyone@gmail.com>
2022-10-10 01:32:03 +0700

    (StructureFirebase): Fixed Models and Create Comments Slice

--- src/pages/Details-Movies/components/Content/TabContent/Comments/ParentComment.tsx
+++ src/pages/Details-Movies/components/Content/TabContent/Comments/ParentComment.tsx
@@ -1,106 +1,91 @@
+import { IConvertComment } from '@src/@types/__global__';
+import { useAuth } from '@src/services/context/Auth';
+import { createReplyDocument } from '@src/services/Firebase/Documents/addDocument';
+import { useAppDispatch } from '@src/services/Store';
 import * as React from 'react';
-import Tippy from '@tippyjs/react/headless';
-import Reaction from './Reaction';
-import { useSpring } from 'framer-motion';
-import { Instance, Props } from 'tippy.js';
+import ChildComment from './ChildComment';
 import InputComment from './InputComment';
-import { IUserData, IComment } from '@src/@types/__Firebase__';
-import { Timestamp } from 'firebase/firestore';
+import ReactionBarWrapper from './ReactionBarWrapper';
+import GuestProfile from '@src/asserts/images/guest_profile.png';
 
 interface IParentCommentProps {
   children?: React.ReactNode;
-  sender: Omit<IUserData, 'recently' | 'watchlist'>;
-  content: string;
-  createAt: Timestamp;
-  down_vote_count: number;
-  up_vote_count: number;
-  love_vote_count: number;
-  sad_vote_count: number;
-  comments?: IComment | null;
+  type: string;
+  movieId: number;
+  comment: IConvertComment;
 }
 
 const ParentComment: React.FunctionComponent<IParentCommentProps> = (props) => {
-  const {
-    children,
-    sender,
-    content,
-    createAt,
-    down_vote_count,
-    up_vote_count,
-    love_vote_count,
-    sad_vote_count,
-    comments,
-  } = props;
+  const auth = useAuth();
+  const { children, type, movieId, comment } = props;
+  const { data, sender, replies } = comment;
   const [isEnableReply, setEnableReply] = React.useState(false);
-  const springConfig = { damping: 15, stiffness: 300 };
-  const initialScale = 0.5;
-  const opacity = useSpring(0, springConfig);
+  const dispatch = useAppDispatch();
 
-  // Handle Event
-  const handleOnMount = () => {
-    opacity.set(1);
-  };
-
-  const handleOnUnMount = (instance: Instance<Props>) => {
-    const cleanup = opacity.onChange((value) => {
-      if (value <= initialScale) {
-        cleanup();
-        instance.unmount();
-      }
-    });
-    opacity.set(0);
-  };
-
+  //Handle Event
   const handleOnEnableReply = (e: React.MouseEvent) => {
     setEnableReply(!isEnableReply);
   };
 
+  const handleOnSubmitReply = async (value: string) => {
+    if (!auth) {
+      return;
+    }
+    // try {
+    await createReplyDocument(type, movieId, auth.uid, '01GEXZZ1Q0W6FSNTWNK7FJ1SXR', value);
+    // } catch (error) {}
+  };
+
+  const handleOnReaction = async (reactionType: string) => {
+    if (!auth) {
+      return;
+    }
+    console.log(reactionType);
+  };
+
   return (
     <li>
-      <div className="relative pb-8">
+      <div className="relative py-4">
         <div className="flex">
           <img
-            src={
-              sender.images.photoURL
-                ? sender.images.photoURL
-                : 'https://giaitri.vn/wp-content/uploads/2019/07/avatar-la-gi-01.jpg'
-            }
-            alt=""
+            src={sender.images.photoURL ? sender.images.photoURL : GuestProfile}
+            alt={`photoURL`}
             className=" object-cover w-14 h-14 rounded-full"
           />
           <div className="mx-4 w-full">
-            <div className="py-2 px-4  bg-dark-smooth-on-surface rounded-lg text-base">
+            <div className="py-2 px-4  bg-dark-smooth-on-surface rounded-lg text-base relative">
               <span className="text-white/80">{sender.information.displayName}</span>
-              <div className="py-2">
-                <p className="text-white/60">{content}</p>
+              <div className="py-2 pb-2">
+                <p className="text-white/60">{data.content}</p>
               </div>
+              {/* <ShowReactions
+                like_count={like_count}
+                dislike_count={dislike_count}
+                love_count={love_count}
+                sad_count={sad_count}
+                reactions={reactions}
+              /> */}
             </div>
-            <div className="text-white/80 inline-flex items-center gap-4 pt-2 px-4">
-              <div>
-                <Tippy
-                  onMount={handleOnMount}
-                  onHide={handleOnUnMount}
-                  animation={true}
-                  offset={[30, 5]}
-                  interactive
-                  delay={[230, 0]}
-                  placement={'bottom'}
-                  render={(attrs) => <Reaction style={{ opacity }} attrs={attrs} />}
-                >
-                  <button onClick={() => console.log('Reaction: from Parent Like')}>Like</button>
-                </Tippy>
-              </div>
 
+            <div className="text-white/80 inline-flex items-center gap-4 pt-4 px-4">
+              <ReactionBarWrapper onReaction={handleOnReaction} />
               <button onClick={handleOnEnableReply} className={`${isEnableReply ? 'text-white/80' : 'text-white/60'}`}>
                 Reply
               </button>
-              <span className="text-sm text-white/60 inline-flex items-center">{createAt.toDate().toDateString()}</span>
+              <span className="text-sm text-white/60 inline-flex items-center">Just now</span>
             </div>
-            {isEnableReply && <InputComment />}
+
+            {isEnableReply && <InputComment onSubmit={handleOnSubmitReply} />}
           </div>
         </div>
       </div>
-      {children && <ul>{children}</ul>}
+      {replies && (
+        <ul>
+          {replies.map((reply) => (
+            <ChildComment type={type} movieId={movieId} reply={reply} />
+          ))}
+        </ul>
+      )}
     </li>
   );
 };
